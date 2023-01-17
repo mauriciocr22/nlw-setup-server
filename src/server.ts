368865df96304b994a8c018d04524a6d3dc6eec7
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-17 20:44:18 -0300

    chore: installed prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get("/", () => {
+  return "Hello NLW";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running");
+});
